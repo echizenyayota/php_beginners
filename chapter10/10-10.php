12a2aa7bee22d1de90d0930fb00c3658f8434fa9
echizenyayota <virtualmisso30@gmail.com>
2017-08-30 16:30:13 +0900

    P206 例10-10

--- chapter10/10-10.php
+++ chapter10/10-10.php
@@ -7,4 +7,8 @@ if (isset($_SESSION['count'])) {
   $_SESSION['count'] = 1;
 }
 
+// var_dump($_SESSION['count']);
+// var_dump($_SESSION['']);
+
 echo "あなたが見たページの回数は" . $_SESSION['count'] . "回です";
+unset($_SESSION['count']);
